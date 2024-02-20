af2f32d89d5a18fdb55d6732da9ad2178350584c
Saritawanjiku <157991202+Saritawanjiku@users.noreply.github.com>
2024-02-20 10:26:20 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,12 @@
+
+#include <stdio.h>
+
+int main() {
+    float floatNumber = 2.285;
+    int integerNumber = (int)floatNumber;
+
+    printf("Float: %f\n", floatNumber);
+    printf("Int: %d\n", integerNumber);
+
+    return 0;
+}
